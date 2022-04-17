0db436fcaf9b0e3ee3eea0b33e7f2cd7cfd7f722
Hardeep Singh <hardeep.singh@eintech.com>
2022-04-17 15:41:46 +0100

    Added showreel

---
+++ src/components/portfolio/PortfolioShowreel.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="waxon_tm_portoflio" id="showreel">
+    
+    <div class="container">
+      <div
+        class="positon-relative waxon_tm_main_title"
+        v-scroll-reveal="{ delay: 40, distance: '30px' }"
+      >
+        <div class="title">
+          <h3>Showreel<span class="bg">Video</span></h3>
+        </div>
+      </div>
+
+      <div class="tabcontent">
+        <div class="portfolio_inner">
+          <div style="padding:56.25% 0 0 0;position:relative;">
+            <iframe src="https://player.vimeo.com/video/697299489?h=9f02be60bd&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;portrait=0&amp;byline=0&amp;title=0" 
+              frameborder="0"
+              allow="autoplay; fullscreen; picture-in-picture" 
+              allowfullscreen 
+              style="position:absolute;top:0;left:0;width:100%;height:100%;" 
+              title="Hardeep monologue - Death of a saleman">
+            </iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+  
+</template>
+
